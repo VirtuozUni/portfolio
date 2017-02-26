bc0d525387d08a08f7990440e72cbeefb806d683
Pavel Miroshin <mps@retailasp.ru>
2017-02-26 22:25:04 +0300

    phone 3th page

--- source/js/app.js
+++ source/js/app.js
@@ -4,7 +4,6 @@ function changePreview(elem,image)
     $(elem).fadeOut(500,function() {$(this).attr('src',image).fadeIn(500)});
 }
 function slider(list,inx) {
-    console
     clearTimeout(sliderTime);
     inx = (inx>=list.length)?0:inx;
     $('.c-slider-buttons__left').data('prev',((inx-1) < 0)?(list.length-1):(inx-1));
@@ -27,16 +26,41 @@ function slider(list,inx) {
 
 }
 
-(function() {
-  'use strict';
+var blur = (function() {
+   var wrapper = document.querySelector('.l-callback__callback-place'),
+       form = document.querySelector('.blur'),
+       bgImgW = 2000,
+       bgImgH = 1699;
+   return {
+       set: function() {
+            var imgWidth = document.querySelector('.l-callback').offsetWidth,
+                posLeft  = -wrapper.offsetLeft,
+                posTop = -wrapper.offsetTop,
+                blurCSS = form.style,
+                imgProc = imgWidth/bgImgW,
+                imgHeight = bgImgH * imgProc,
+                offsetH = document.querySelector('.l-callback').offsetHeight,
+                realTop =  offsetH - imgHeight + posTop;
+            console.log(posTop,imgWidth,imgProc,imgHeight,offsetH,realTop );
+            blurCSS.backgroundSize = imgWidth + 'px' + ' auto';
+            blurCSS.backgroundPosition = posLeft + 'px' + ' ' + realTop + 'px';
+       }
+   }
+}());
+window.onresize = function(){
+    blur.set();
+}
+$(document).ready(function(){
     var sliderList = ['work-1.png','work-2.png','work-3.png','work-4.png'];
     $('.c-slider-buttons__left').data('prev',sliderList.length-1)
         .click(function() {slider(sliderList,$(this).data('prev'));});
+    console.log('/assets/img/slider/'+sliderList[$('.c-slider-buttons__left').data('prev')]);
     $('.l-left__preview').attr('src','/assets/img/slider/'+sliderList[$('.c-slider-buttons__left').data('prev')]);
     $('.c-slider-buttons__right').data('next',1)
         .click(function() {slider(sliderList,$(this).data('next'));});
     $('.l-right__preview').attr('src','/assets/img/slider/'+sliderList[$('.c-slider-buttons__right').data('next')]);
 
     sliderTime = setTimeout(function(){slider(sliderList,1)},7000);
+    blur.set();
 
-})();
+});
